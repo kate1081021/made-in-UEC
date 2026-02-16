979e0b3298804517b5b056a086c223c1714d54ad
peachamomomo <morimotoai2004@gmail.com>
2026-02-16 14:21:43 +0900

    ウィンドウとカーソルのオブジェクト、及びカーソル入力によるウィンドウ消滅機能を仮実装 仕様確定次第編集予定

---
+++ Assets/WI/Script/WI_M_templete.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace WI 
+{
+
+    //”CˆÓ‚ÌƒNƒ‰ƒX–¼‚É•ÏX‚µ‚Ä‚­‚¾‚³‚¢
+    public class WI_M_templete : MiniGameBase 
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public override void OnGameStart() 
+        {
+
+        }
+
+        public override void OnGameEnd() { }
+
+        // Update is called once per frame
+        void Update() 
+        {
+
+        }
+    }
+}
+
