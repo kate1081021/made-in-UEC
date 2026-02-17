a09e84bc15776e93b54eb1c48118f5ea69045d98
Jun000042 <jack0000422>
2026-02-17 17:36:28 +0900

    プレゼント操作 プレゼントが操作できるようになりました。

--- Assets/PT/PT_Manager.cs
+++ Assets/PT/PT_Manager.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
-using System.Numerics;
-using System;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
@@ -8,15 +7,18 @@ namespace PTgame
 {
     public class PT_Manager : MiniGameBase
     {        
-        public List<GameObject> presents = new List<GameObject>(); //プレゼント
-        public int present_count;
-        public float present_slope;
-        public GameObject present;
+        [SerializeField] private List<GameObject> presents = new List<GameObject>(); //プレゼント
+        [SerializeField] private int present_count;
+        [SerializeField] private float present_slope;
+
+        [SerializeField] private GameObject present_parent;
+        [SerializeField] private GameObject present;
+        [SerializeField] private GameObject move;
         public override void OnGameStart()
         {
             MGManager.Load();
             present_count = 5;
-            present_slope = UnityEngine.Random.Range(-1f, 1f);
+            present_slope = UnityEngine.Random.Range(-0.5f, 0.5f);
             for (int i = 0; i < present_count; i++)
             {
                 GameObject g = Instantiate(present);
@@ -27,12 +29,15 @@ namespace PTgame
 
         void Update()
         {
-            present_slope += Time.deltaTime*present_slope/10;
+            present_slope += Time.timeScale*presents[1].transform.position.x*(float)(present_count/500f);
+            present_slope += Time.timeScale*move.transform.position.x/100;
+            Debug.Log("+"+(float)(present_count/100f));
             
             for (int i = 0; i < presents.Count; i++)
             {
                 GameObject g = presents[i];
-                g.transform.position = new UnityEngine.Vector3 (present_slope*i, i, 0);
+                g.transform.position = new Vector3 (present_slope*i, i, 0);
+                Debug.Log("max"+present_slope);
             }
 
             if (present_slope*present_slope >= 1)
