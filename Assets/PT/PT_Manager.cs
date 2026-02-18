061172735261fc5f06ec81c31e48341554cb5284
Jun000042 <jack0000422>
2026-02-18 22:59:34 +0900

    速度調整 スピードの設定がおかしかったので直しました

--- Assets/PT/PT_Manager.cs
+++ Assets/PT/PT_Manager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using TMPro;
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 namespace PTgame
@@ -11,21 +12,24 @@ namespace PTgame
         [SerializeField] private List<GameObject> presents = new List<GameObject>(); //プレゼント
         [SerializeField] private int present_count;
         [SerializeField] private float present_slope;
-        [SerializeField] private float present_parent_move;
         [SerializeField] private float ppm_x;
         [SerializeField] public bool fall;
         [SerializeField] private GameObject present_parent;
         [SerializeField] private GameObject present;
         [SerializeField] private PT_move move;
         [SerializeField] private Camera camera;
+        [SerializeField] private int testlevel;
         public override void OnGameStart()
         {
+            MGManager.TestPlay(testlevel);
             MGManager.Load();
-            //present_count = 5;
-            camera.orthographicSize = 2f + (float)present_count / 1.25f;
+            present_count = 5 + (int)((Time.timeScale-1) * 30);
+            camera.orthographicSize = 2f + present_count / 1.25f;
             camera.transform.position = new Vector3(0, -2.5f + (float)present_count / 2, -10);
-            present_parent_move = 0;
-            present_slope = UnityEngine.Random.Range(-0.25f, 0.25f);
+            if (Time.timeScale < 1.25f)
+                present_slope = UnityEngine.Random.Range(-1.25f + Time.timeScale, 1.25f - Time.timeScale);
+            else
+                present_slope = UnityEngine.Random.Range(-0.01f, 0.01f);;
             fall = false;
             for (int i = 0; i < present_count; i++)
             {
@@ -55,9 +59,11 @@ namespace PTgame
         {
             //Debug.Log("local" + presents[1].transform.localPosition);
             //Debug.Log("present_slope"+ present_slope +"delta" + (movex*0.01f));
+            float time = Time.timeScale * Time.deltaTime;
             if (present_slope * present_slope < 1)
             {
-                present_slope = Time.timeScale * (presents[1].transform.localPosition.x + presents[1].transform.localPosition.x * (float)present_count / 500f);
+                present_slope = presents[1].transform.localPosition.x + presents[1].transform.localPosition.x * (float)present_count / 10 * time;
+                //Debug.Log("1.00 "+present_slope+" vs "+Time.timeScale+" "+(present_slope*Time.timeScale));
                 //present_slope += Time.timeScale*move.transform.position.x/500f;
                 float movex = move.HandleMove().x;
 
@@ -65,29 +71,29 @@ namespace PTgame
                 {
                     if (ppm_x <= 0)
                     {
-                        ppm_x -= Time.timeScale / 10;
+                        ppm_x -= time * 10;
                     }
                     else if (ppm_x > 0)
                     {
-                        ppm_x = ppm_x / 10 - Time.timeScale / 10;
+                        ppm_x = ppm_x / (10 * Time.timeScale) - time * 10;
                     }
                 }
                 else if (movex > 0)
                 {
                     if (ppm_x >= 0)
                     {
-                        ppm_x += Time.timeScale / 10;
+                        ppm_x += time * 10;
                     }
                     else if (ppm_x < 0)
                     {
-                        ppm_x = ppm_x / 10 + Time.timeScale / 10;
+                        ppm_x = ppm_x / (10 * Time.timeScale) + time * 10;
                     }
                 }
                 else
                 {
                     if (ppm_x * ppm_x > 0.0001f)
                     {
-                        ppm_x = ppm_x / 1.2f;
+                        ppm_x = ppm_x / (1.2f * Time.timeScale);
                     }
                     else
                     {
@@ -100,11 +106,11 @@ namespace PTgame
                 for (int i = 0; i < presents.Count; i++)
                 {
                     GameObject g = presents[i];
-                    float power = (float)Math.Atan((float)present_count/60);
+                    float power = (float)Math.Atan((float)present_count / 100);
                     if (power > 0.1f)
-                        power = 0.1f;                
+                        power = 0.1f;
                     //Debug.Log("power"+power);
-                    g.transform.localPosition = new Vector3(Time.timeScale * (present_slope - movex * 4 * power) * i, i, 0);
+                    g.transform.localPosition = new Vector3((present_slope - movex * 4 * power) * i, i, 0);
                 }
             }
             else
@@ -114,7 +120,7 @@ namespace PTgame
 
                 if (ppm_x * ppm_x > 0.0001f)
                 {
-                    ppm_x = ppm_x / 1.05f;
+                    ppm_x = ppm_x / (1.05f * Time.timeScale);
                 }
                 else
                 {
