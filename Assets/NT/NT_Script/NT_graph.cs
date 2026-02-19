910d4d950355fcc55679da7014ede708597c2758
moon8458 <hazuki.huahua@icloud.com>
2026-02-20 01:30:47 +0900

    滴定曲線の追加

---
+++ Assets/NT/NT_Script/NT_graph.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace NT
+{
+    public class NT_graph : MiniGameBase
+    {
+        private int count;
+        [SerializeField] private Image image;
+        [SerializeField] private NT_switch switchComponent;
+
+
+        public override void OnGameStart()
+        {
+            MGManager.Load();
+        }
+        void Update()
+        {
+            count = switchComponent.count;
+            image.fillAmount = (float)count / 350;
+            image.fillAmount = Mathf.Clamp01(image.fillAmount);
+        }
+        public override void OnGameEnd()
+        {
+            
+        }
+    }
+}
