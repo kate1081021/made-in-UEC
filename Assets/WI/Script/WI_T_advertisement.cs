907a068fa8d8262db186db857897df480151a402
25Tsubouchi <t2511164@gl.cc.uec.ac.jp>
2026-02-21 21:11:45 +0900

    WI_T_advertisement.cs の中身を少し変更しました

--- Assets/WI/Script/WI_T_advertisement.cs
+++ Assets/WI/Script/WI_T_advertisement.cs
@@ -2,8 +2,14 @@ using UnityEngine;
 
 namespace WI
 {
-    public class WI_T_advertisement : MonoBehaviour
+    public class WI_T_advertisement : MiniGameBase 
     {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public override void OnGameStart() { }
+
+        public override void OnGameEnd() { }
+        void Update() { }
+
         public void CloseWindow()
         {
             Destroy(this.gameObject);
