0f14b7698cc81ebc6834e357f278f5e7d35de65f
25Tsubouchi <t2511164@gl.cc.uec.ac.jp>
2026-02-21 19:16:13 +0900

    広告タイプのウィンドウを追加

---
+++ Assets/WI/Script/WI_T_adClose.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+
+namespace WI
+{
+    public class WI_T_adClose : MiniGameBase
+    {
+        private BoxCollider2D cursorCollider;
+        private bool isCursorHovering = false;
+        private WI_T_advertisement parentWindow;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        public override void OnGameStart()
+        {
+            GameObject cursorObj = GameObject.Find("WI_M_cursor");
+            if (cursorObj != null)
+            {
+                cursorCollider = cursorObj.GetComponent<BoxCollider2D>();
+            }
+
+            parentWindow = this.transform.parent.GetComponent<WI_T_advertisement>();
+        }
+
+        public override void OnGameEnd() { }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isCursorHovering && Action.WasPerformedThisFrame())
+            {
+                if (parentWindow != null)
+                {
+                    parentWindow.CloseWindow();
+                }
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (cursorCollider != null && collision == cursorCollider)
+            {
+                isCursorHovering = true;
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            if (cursorCollider != null && collision == cursorCollider)
+            {
+                isCursorHovering = false;
+            }
+        }
+    }
+}
---
+++ Assets/WI/Script/WI_T_advertisement.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace WI
+{
+    public class WI_T_advertisement : MonoBehaviour
+    {
+        public void CloseWindow()
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
