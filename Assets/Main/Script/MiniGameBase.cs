0d3ff3de100c2deff23ece195c19271f2b6d291b
Ryu_Devloper <k2511076>
2026-02-22 19:02:09 +0900

    Revert "Merge pull request #41 from kate1081021/Ryusei"
    This reverts commit c415bc0de9bc1636b62ca547541b49748f47124a, reversing
    changes made to 081eb2667bd47a2829e0003f466cdb859b0eb241.

--- Assets/Main/Script/MiniGameBase.cs
+++ Assets/Main/Script/MiniGameBase.cs
@@ -55,8 +55,6 @@ public abstract class MiniGameBase : BaseScript, IMiniGame
         Action.canceled += OnAction;
 
         OnGameStart();
-        if (gameBGM != null) { BGMPlay(); }
-        
     }
 
     public void OnMove(InputAction.CallbackContext ctx)
