361b8e0ed5a83df2e56b4a4b244c4e2715da3abc
kiharu <yugo200412@gmai.com>
2026-02-23 23:33:46 +0900

    再コンフリクト解消のため再マージ

--- Assets/NT/NT_Script/NT_switch.cs
+++ Assets/NT/NT_Script/NT_switch.cs
@@ -8,13 +8,24 @@ public class NT_switch : MiniGameBase
 {
     public TMP_Text countText;
     public float count = 0;
+<<<<<<< HEAD
+    AudioSource audioSource;
+    [SerializeField] private float sounddistance;
+    [SerializeField] private float soundborder;
+    int i = 0;
+
+=======
     public Stopwatch sw = new Stopwatch();
+>>>>>>> 10fb74610cf612ed4f4aaa19b2e99561160933db
 
     public override void OnGameStart(){
-        MGManager.TestPlay(100);
     MGManager.Load();
     count = 0;
+<<<<<<< HEAD
+    audioSource = GetComponent<AudioSource>();
+=======
     sw.Start();
+>>>>>>> 10fb74610cf612ed4f4aaa19b2e99561160933db
     }
 
  public override void OnGameEnd(){}
@@ -24,8 +35,18 @@ public class NT_switch : MiniGameBase
         if (Action.IsPressed())
         {
             count=count+Time.timeScale;
+            if (countText != null){
             countText.text = "" + count;
+<<<<<<< HEAD
+            }
+        }
+        if (audioSource != null && count >= soundborder + i*sounddistance)
+        {
+            audioSource.Play();
+            i++;
+=======
             sw.Restart();
+>>>>>>> 10fb74610cf612ed4f4aaa19b2e99561160933db
         }
     }
 }
