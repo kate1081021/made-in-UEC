10fb74610cf612ed4f4aaa19b2e99561160933db
moon8458 <hazuki.huahua@icloud.com>
2026-02-23 16:12:19 +0900

    即クリアの追加

--- Assets/NT/NT_Script/NT_switch.cs
+++ Assets/NT/NT_Script/NT_switch.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Diagnostics;
 
 namespace NT
 {
@@ -7,12 +8,13 @@ public class NT_switch : MiniGameBase
 {
     public TMP_Text countText;
     public float count = 0;
-
+    public Stopwatch sw = new Stopwatch();
 
     public override void OnGameStart(){
         MGManager.TestPlay(100);
     MGManager.Load();
     count = 0;
+    sw.Start();
     }
 
  public override void OnGameEnd(){}
@@ -23,6 +25,7 @@ public class NT_switch : MiniGameBase
         {
             count=count+Time.timeScale;
             countText.text = "" + count;
+            sw.Restart();
         }
     }
 }
