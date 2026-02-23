08a7d29f108af65c1c521bab1256fb616f5e60f1
94aki <k2511084@gl.cc.uec.ac.jp>
2026-02-23 23:00:24 +0900

    音を追加しました

--- Assets/NT/NT_Script/NT_switch.cs
+++ Assets/NT/NT_Script/NT_switch.cs
@@ -7,12 +7,16 @@ public class NT_switch : MiniGameBase
 {
     public TMP_Text countText;
     public float count = 0;
+    AudioSource audioSource;
+    [SerializeField] private float sounddistance;
+    [SerializeField] private float soundborder;
+    int i = 0;
 
 
     public override void OnGameStart(){
-        MGManager.TestPlay(100);
     MGManager.Load();
     count = 0;
+    audioSource = GetComponent<AudioSource>();
     }
 
  public override void OnGameEnd(){}
@@ -22,7 +26,14 @@ public class NT_switch : MiniGameBase
         if (Action.IsPressed())
         {
             count=count+Time.timeScale;
+            if (countText != null){
             countText.text = "" + count;
+            }
+        }
+        if (audioSource != null && count >= soundborder + i*sounddistance)
+        {
+            audioSource.Play();
+            i++;
         }
     }
 }
