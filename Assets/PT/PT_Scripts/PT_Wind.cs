78da71c7539f220836337ccac399bd950ee9cea8
Jun000042 <jack0000422>
2026-02-28 00:34:32 +0900

    Revert "Merge branch 'Present_Junya' into Present"
    This reverts commit 04d69f178ac96c718799a04b15190233b1077876, reversing
    changes made to 8b122b2390400a9230e174fda51f648d529fa723.

--- Assets/PT/PT_Scripts/PT_Wind.cs
+++ Assets/PT/PT_Scripts/PT_Wind.cs
@@ -13,6 +13,9 @@ namespace PTgame
         [SerializeField] private float time;
         [SerializeField] private Camera game_camera;
 
+        // パーティクル書き換え用の配列キャッシュ
+        private ParticleSystem.Particle[] particlesArray;
+
         void Awake()
         {
             if (game_camera == null) game_camera = Camera.main;
