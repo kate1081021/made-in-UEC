f1ed41f368851b7917de9b0eedc4c16e22ca6e75
25kanaizuka <rk06151312005@gmail.com>
2026-02-28 17:58:23 +0900

    導火線の演出の実装

--- Assets/LF/Shiryu/scripts/LF_fire.cs
+++ Assets/LF/Shiryu/scripts/LF_fire.cs
@@ -6,7 +6,11 @@ public class LF_fire : MonoBehaviour
 {
     [Header("諸設定")]
     public Transform FireGoal;
-    public float throwDuration = 0.5f;
+    public Transform FireRestart;
+    public GameObject bomb;
+    public GameObject match;
+    public float fireDuration = 0.5f;
+    public float lastDuration = 0.2f;
 
     public void StartFire(){
         StartCoroutine(FireMovie());
@@ -15,14 +19,30 @@ public class LF_fire : MonoBehaviour
     private IEnumerator FireMovie(){
 
         Vector3 startPos = transform.position;
-        
+
         // 座標の移動
         float elapsed = 0;
-        while (elapsed < throwDuration){
+        while (elapsed < fireDuration){
             elapsed += Time.deltaTime;
-            float t = elapsed / throwDuration;
+            float t = elapsed / fireDuration;
 
             transform.position = Vector3.Lerp(startPos, FireGoal.position, t);
+
+            yield return null;
+        }
+
+        Vector3 lastgoal = new Vector3(bomb.transform.position.x,startPos.y,transform.position.z);
+
+        transform.position = FireRestart.position;
+
+        elapsed = 0;
+        startPos = transform.position;
+        
+        while (elapsed < lastDuration){
+            elapsed += Time.deltaTime;
+            float t = elapsed / lastDuration;
+
+            transform.position = Vector3.Lerp(startPos, lastgoal, t);
 
             yield return null;
         }
