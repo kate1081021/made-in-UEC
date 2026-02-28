97661f46d73923f4c9c41a136f3318e137e00ab0
Neko-Rintaro <o2511056@gl.cc.uec.ac.jp>
2026-02-28 13:19:33 +0900

    再コミットします

--- Assets/MIU_InputSystem.cs
+++ Assets/MIU_InputSystem.cs
@@ -251,28 +251,6 @@ public partial class @MIU_InputSystem: IInputActionCollection2, IDisposable
                 },
                 {
                     ""name"": """",
-                    ""id"": ""6e574e6d-0b03-4d4b-95f3-0f246eb7c959"",
-                    ""path"": ""<Gamepad>/leftStick"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""ec323528-e61e-4d23-830f-e51e8ec1c2db"",
-                    ""path"": ""<Gamepad>/dpad"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Move"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
                     ""id"": ""99bd8d9b-7d55-499b-a702-699d9f755994"",
                     ""path"": ""<Keyboard>/space"",
                     ""interactions"": """",
@@ -286,17 +264,6 @@ public partial class @MIU_InputSystem: IInputActionCollection2, IDisposable
                     ""name"": """",
                     ""id"": ""5ecc284d-aca2-4121-a343-e5061904320a"",
                     ""path"": ""<Keyboard>/enter"",
-                    ""interactions"": """",
-                    ""processors"": """",
-                    ""groups"": """",
-                    ""action"": ""Action"",
-                    ""isComposite"": false,
-                    ""isPartOfComposite"": false
-                },
-                {
-                    ""name"": """",
-                    ""id"": ""b2ed8381-b77a-4d3d-8c13-f98567131c4d"",
-                    ""path"": ""<Gamepad>/buttonSouth"",
                     ""interactions"": """",
                     ""processors"": """",
                     ""groups"": """",
