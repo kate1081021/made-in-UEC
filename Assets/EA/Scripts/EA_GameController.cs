c5a5777cbbd8168ed0db34e1ef64021e841fa8bf
Ryu_Devloper <k2511076>
2026-03-13 02:52:00 +0900

    カーソルで移動できるようにした。

--- Assets/EA/Scripts/EA_GameController.cs
+++ Assets/EA/Scripts/EA_GameController.cs
@@ -57,6 +57,8 @@ namespace EA
         /// </summary>
         public EA_CarsorView carsolView;  // カーソルの見た目
         public GameObject carsol;  // カーソルオブジェクト
+        private int carsor_x = 0;
+        private int carsor_y = 0;
 
         /// <summary>
         /// UI関連
@@ -84,6 +86,7 @@ namespace EA
             {
                 for (int j = 0; j < l; j++)
                 {
+                    // currentBoard
                     GameObject cell = Instantiate(cellPrefab);  // プレファブを生成する
 
                     Transform transform = cell.GetComponent<Transform>();  
@@ -92,12 +95,70 @@ namespace EA
                     EA_CellView cellView = cell.GetComponent<EA_CellView>();
                     cellView.Flip(boardModel.GetCell(j, i));
                     cells[i, j] = cell;
+
+                    // targetBoard
+                    GameObject cell2 = Instantiate(cellPrefab);  // プレファブを生成する
+                    Transform transform2 = cell2.GetComponent<Transform>();  
+                    transform2.position = new Vector3(3.25f + (((8 - l) / 2 + j) * 0.5f), -0.25f - (((8 - l) / 2 + i)) * 0.5f, -1);
+                    transform2.localScale = new Vector3(0.5f, 0.5f, 1);
+
+                    EA_CellView cellView2 = cell2.GetComponent<EA_CellView>();
+                    cellView2.Flip(boardModel.GetTargetCell(j, i));
+
                 }
             }
+
+            // カーソルを移動する
+            carsolView.CarsorMove(0, 0, l);
 
             // ミニゲームを開始する
             MGManager.Load();
 
+        }
+
+        // 上下左右キーが押されている
+        protected override void OnMovePerformed(Vector2 value)
+        {
+            Vector2 val = convert_stick_to_dir(value);
+
+            // x座標
+            carsor_x = (carsor_x + (int)val.x) % boardModel.boardLength;
+            if (carsor_x < 0) { carsor_x += boardModel.boardLength; }
+            Debug.Log(carsor_x);
+
+            // y座標
+            carsor_y = (carsor_y - (int)val.y) % boardModel.boardLength;
+            if (carsor_y < 0) { carsor_y += boardModel.boardLength; }
+            Debug.Log(carsor_y);
+
+            // カーソルを動かす
+            carsolView.CarsorMove(carsor_x, carsor_y, boardModel.boardLength);
+
+        }
+
+        // Enter/Spaceキーが押された
+        protected override void OnActionStarted(float value)
+        {
+            // 盤面でひっくり返す処理を行う
+            boardModel.Flip(carsor_x, carsor_y);
+
+            // 見た目の処理
+            for (int i = carsor_y - 1; i <= carsor_y + 1; i++)
+            {
+                // iがyの範囲からそれている場合
+                if (i < 0 || boardModel.boardLength <= i) { continue; }
+
+                for (int j = carsor_x - 1; j <= carsor_x + 1; j++)
+                {
+                    // jがxの範囲からそれている場合
+                    if (j < 0 || boardModel.boardLength <= j) { continue; }
+
+                    // ひっくり返す
+                    bool side = boardModel.GetCell(j, i);
+                    cells[i, j].GetComponent<EA_CellView>().Flip(side);
+
+                }
+            }
         }
 
         // デモプレイ(1回だけ呼ばれる)
